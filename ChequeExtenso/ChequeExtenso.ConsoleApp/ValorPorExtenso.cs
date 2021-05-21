0fb37c3346480127328f1e227570749f8fb222e5
Bleyk <50923316+pedro-ca@users.noreply.github.com>
2021-05-21 17:02:58 -0300

    Initial Commit

---
+++ ChequeExtenso/ChequeExtenso.ConsoleApp/ValorPorExtenso.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChequeExtenso.ConsoleApp
+{
+    public class ValorPorExtenso
+    {
+        private double valor;
+        private string moeda;
+
+        public ValorPorExtenso(double valor, string moeda)
+        {
+            this.valor = valor;
+            this.moeda = moeda;
+        }
+
+        public string ConverterParaExtenso()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ChequeExtenso/ChequeExtenso.UnitTest/ValorPorExtensoTest.cs
@@ -0,0 +1,10 @@
+using NUnit.Framework;
+using ChequeExtenso.ConsoleApp;
+
+namespace ChequeExtenso.UnitTest
+{
+    public class ValorPorExtensoTest
+    {
+        
+    }
+}
